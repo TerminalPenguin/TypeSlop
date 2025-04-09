d89603bf7867160c16f525d2bcdc304d28cffe29
TerminalPenguin <jenishdhakal456@gmail.com>
2025-04-09 07:10:47 +0000

    ok added ACTUAl stuff this time

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
-for i in range(3):
-	print(i)
+# ptr <string> (print)
+# ptr* var (Should prob be setup after variables)
+# setting variables: "set <type> <name> <value>"
+# types: string, int, float
+variables = {'mystring':'hi'}
+with open('main.txt') as f:
+	lines = f.readlines()
+	for line in lines:
+		if line.startswith('prt'):
+			prtsplit = line.split(' ')
+			del prtsplit[0]
+			prtvalue = ' '.join(prtsplit)
+			print(prtvalue)
+		if line.startswith('prt*'):
+			prtsplit = line.split(' ')
+		if line.startswith('set'):
+			setsplit = line.split(' ')
+			print(setsplit[2])
+			varType = setsplit[1]
+			varName = setsplit[2]
+			if varType == 'string':
+				del setsplit[0:3]
+				stringValue = ' '.join(setsplit)
+				print(stringValue)
+				variables[varName] = stringValue
+				print(variables)
+			else:
+				print(f"error occured:'  {line}  ' set variable type {varType} is not valid type")
